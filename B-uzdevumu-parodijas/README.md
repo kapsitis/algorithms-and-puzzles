e72e5a62bc3517ef8cb11eabbc0411f75623271b
kapsitis <kalvis.apsitis@gmail.com>
2021-10-24 01:24:22 +0300

    B-uzdevumu parodijas

--- B-uzdevumu-parodijas/README.md
+++ B-uzdevumu-parodijas/README.md
@@ -1,5 +1,17 @@
 # Algoritmi un programmēšana
 
+## Kalendāra uzdevums
+
+Izveidot programmu gan C++, gan Python, obligāti izmantojot norādītās vai kādas citas funkcijas.
+Prasības tādas pašas kā iepriekšējā uzdevumā, precīzāk sk. Laboratorijas darbu noteikumos.
+
+Dots datums no trim skaitļiem: YYYY, MM un DD.
+(Piemēram, 2021. g. 21.oktobris ir trīs skaitļi: 2021, 10 un 21.)
+Dots dienu skaits N. Uzrakstīt funkciju, lai atrastu to datumu,
+kas būs tieši pēc N dienām.
+
+Implementāciju sk. šajā repozitorijā: **calendar.cpp*
+
 Testa fails **calendar-test01.txt**:
 
 ``` 
@@ -38,4 +50,67 @@ Sagaidāmā izvade **calendar-expected01.txt**
 2018-12-26
 2022-2-28
 2024-2-29
+```
+
+
+## Šaha zirdziņa uzdevums
+
+Izveidot programmu gan C++, gan Python, obligāti izmantojot norādītās vai kādas citas funkcijas.
+Prasības tādas pašas kā iepriekšējā uzdevumā, precīzāk sk. Laboratorijas darbu noteikumos.
+
+Doti divi skaitļi no 1 līdz 8 (šaha galdiņa horizontāle un vertikāle).
+Uzzīmēt šaha galdiņu, kur norādītajā lauciņā novietots šaha zirdziņš un
+atzīmēti tie lauciņi, kurus šaha zirdziņš apdraud.
+
+Izveidot šim nolūkam funkciju, kura četrām norādītām koordinātēm
+x1, y1, x2, y2 - atgriež "true" tad un tikai tad, ja no (x1,y1) uz
+(x2,y2) var aiziet ar vienu zirdziņa gājienu.
+
+
+Testa fails **chess-test01.txt**: 
+
+```
+3 5
+1
+1 1
+1
+7 2
+0
+```
+
+Sagaidāmais rezultāts **chess-expected01.txt**: 
+
+```
+   A B C D E F G H
+8  . . . . . . . .
+7  . * . * . . . .
+6  * . . . * . . .
+5  . . K . . . . .
+4  * . . . * . . .
+3  . * . * . . . .
+2  . . . . . . . .
+1  . . . . . . . .
+   A B C D E F G H
+
+   A B C D E F G H
+8  . . . . . . . .
+7  . . . . . . . .
+6  . . . . . . . .
+5  . . . . . . . .
+4  . . . . . . . .
+3  . * . . . . . .
+2  . . * . . . . .
+1  K . . . . . . .
+   A B C D E F G H
+
+   A B C D E F G H
+8  . . . . . . . .
+7  . . . . . . . .
+6  . . . . . . . .
+5  . . . . . . . .
+4  . . . . . * . *
+3  . . . . * . . .
+2  . . . . . . K .
+1  . . . . * . . .
+   A B C D E F G H
 ```
--- B-uzdevumu-parodijas/chess.cpp
+++ B-uzdevumu-parodijas/chess.cpp
@@ -1,61 +1,68 @@
+/*
+Izveidot programmu gan C++, gan Python, obligāti izmantojot norādītās vai kādas citas funkcijas.
+Prasības tādas pašas kā iepriekšējā uzdevumā, precīzāk sk. Laboratorijas darbu noteikumos.
+
+Doti divi skaitļi no 1 līdz 8 (šaha galdiņa horizontāle un vertikāle).
+Uzzīmēt šaha galdiņu, kur norādītajā lauciņā novietots šaha zirdziņš un
+atzīmēti tie lauciņi, kurus šaha zirdziņš apdraud.
+
+Izveidot šim nolūkam funkciju, kura četrām norādītām koordinātēm
+x1, y1, x2, y2 - atgriež "true" tad un tikai tad, ja no (x1,y1) uz
+(x2,y2) var aiziet ar vienu zirdziņa gājienu.
+*/
+
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-bool isGaraisGads(int yyyy) {
-    //if (yyyy % 4 == 0 ) {return true;}
-    //else return false;
-
-    return (yyyy % 4 == 0 && yyyy % 100 != 0);
-}
-
-// ciparu skaits
-int f2(int arg) {
-    if (arg == 0) return 1;
-    int count = 0; 
-    while (arg > 0) {
-        arg = arg /10; 
-        count ++; 
-    }
-    return count;
-}
-
-double f3(int xA, int yA, int xB, int yB) {
-    return (sqrt( (xB - xA)*(xB - xA) + (yB - yA)* (yB - yA) ));
-}
-
-// atgriež true tad un tikai tad, ja no laucina 
+// Atgriež true tad un tikai tad, ja no laucina
 // (x1,y1) uz laucinu (x2,y2) var aiziet ar vienu zirdzina gajienu
-bool f4(int x1, int y1, int x2, int y2) {
-    return (abs(x1-x2) * abs(y1 - y2) == 2); // 1*2 == 2, 2*1 == 2. 
+bool f4(int x1, int y1, int x2, int y2)
+{
+    return (abs(x1 - x2) * abs(y1 - y2) == 2); // 1*2 == 2, 2*1 == 2.
 }
 
 int main()
 {
-    int y1 = 0; 
-    cout << f2(y1) << endl;
-    int y2 = 12341243; 
-    cout << f2(y2) << endl;
-
-    int xA = 0, yA = 0, xB = 4, yB = 5; // (4-0)^2  + (5 - 0)^2 = 16 + 25 = 41
-    cout << "Attālums ir " << f3(xA, yA, xB, yB) << endl; 
-    // (0;0) - (4;5)
-
-    int x1 = 3; y1 = 5; 
-    int a[8][8] = {0};
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            if (f4(x1,y1, i,j)) {
-                a[i][j] = 1;
+    int turpinat;
+    do
+    {
+        cout << "Ievadiet šaha lauciņa koordinātes (no 1 1 līdz 8 8))" << endl;
+        int x1, y1;
+        cin >> x1 >> y1;
+        // inicializē masīvu ar nullēm.
+        int a[8][8] = {0};
+        // apstaigā horizontāles un katrā horizontālē - vertikāles.
+        for (int j = 1; j <= 8; j++)
+        {
+            for (int i = 1; i <= 8; i++)
+            {
+                if (f4(x1, y1, i, j))
+                {
+                    a[i-1][j-1] = 1;
+                }
             }
         }
-    }
 
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
-            cout << ((a[i][j] == 1)? ". ": "* ");
+        cout << "   A B C D E F G H" << endl;
+        for (int j = 8; j >= 1; j--)
+        {
+            cout << j << "  "; 
+            for (int i = 1; i <= 8; i++)
+            {
+                if (i == x1 && j == y1)
+                {
+                    cout << "K ";
+                }
+                else
+                {
+                    cout << ((a[i-1][j-1] == 1) ? "* " : ". ");
+                }
+            }
+            cout << endl;
         }
+        cout << "   A B C D E F G H" << endl;
         cout << endl;
-    }
-
+        cout << "Vai turpināt (0 - nē, 1 - jā)" << endl;
+        cin >> turpinat;
+    } while (turpinat);
 }
